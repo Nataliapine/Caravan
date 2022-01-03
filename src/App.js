a25c041cca679ce54106d927a93d1a3a32e76ae1
Nataliapine <nataliapine@icloud.com>
2022-01-03 10:43:46 -0300

    create page Locais

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Home from './pages/Home';
+import Locais from './pages/Locais';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/locais" element={<Locais />} />
       </Routes>
     </BrowserRouter>
     
---
+++ src/pages/Locais/index.js
@@ -0,0 +1,90 @@
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+import Foto1 from '../../assets/img/local/foto-1.jpg';
+import Foto2 from '../../assets/img/local/foto-2.jpg';
+import Foto3 from '../../assets/img/local/foto-3.jpg';
+import Foto4 from '../../assets/img/local/foto-4.jpg';
+import Foto5 from '../../assets/img/local/foto-5.jpg';
+import Foto6 from '../../assets/img/local/foto-6.jpg';
+import Foto7 from '../../assets/img/local/foto-7.jpg';
+
+import { Link } from "react-router-dom";
+
+export default function Home() {
+    return (
+        <div>
+            <Header />
+            
+            <section className="container">
+                <div className="text-center my-5">
+                    <span className="h-6 d-block">O MELHOR DESTINO DA SUA VIAGEM</span>
+                    <h1 className="display-4 text-primary">Descubra Dublin</h1>
+                </div>
+                <div className="row">
+                    <div className="col-md">
+                        <img className="img-fluid mb-4" src={Foto1} alt="foto-1" />
+                        <img className="img-fluid mb-4" src={Foto2} alt="foto-2" />
+                        <img className="img-fluid mb-4" src={Foto3} alt="foto-3" />
+                    </div>
+                    <div className="col-md">
+                        <img className="img-fluid mb-4" src={Foto4} alt="foto-4" />
+                        <img className="img-fluid mb-4" src={Foto5} alt="foto-5" />
+                    </div>
+                    <div className="col-md">
+                        <img className="img-fluid mb-4" src={Foto6} alt="foto-6" />
+                        <img className="img-fluid mb-4" src={Foto7} alt="foto-7" />
+                    </div>
+                </div>
+                <div className="text-center my-5">
+                    <span className="h-6 d-block">DESCUBRA O QUE ACONTECE NA CIDADE</span>
+                    <h1 className="display-4 text-primary">Próximos Eventos</h1>
+                </div>
+                <table class="table table-hover my-5 table-responsive-md">
+                    <thead>
+                        <tr>
+                            <th scope="col">Data</th>
+                            <th scope="col">Evento</th>
+                            <th scope="col">Local</th>
+                            <th scope="col">Ticket</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">23/10</th>
+                            <td>Julien Baker</td>
+                            <td>The Academy</td>
+                            <td> <Link to="#" className="btn btn-outline-secondary btn-sm">Comprar Ticket</Link></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">25/10</th>
+                            <td>Lucy Rose</td>
+                            <td>Olympia</td>
+                            <td> <Link to="#" className="btn btn-outline-secondary btn-sm">Comprar Ticket</Link></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">27/10</th>
+                            <td >Bon Iver</td>
+                            <td >Whelans</td>
+                            <td> <Link to="#" className="btn btn-outline-secondary btn-sm">Comprar Ticket</Link></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">27/10</th>
+                            <td >Bon Iver</td>
+                            <td >Whelans</td>
+                            <td> <Link to="#" className="btn btn-outline-secondary btn-sm">Comprar Ticket</Link></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">27/10</th>
+                            <td >Bon Iver</td>
+                            <td >Whelans</td>
+                            <td> <Link to="#" className="btn btn-outline-secondary btn-sm">Comprar Ticket</Link></td>
+                        </tr>
+                    </tbody>
+                    </table>
+            </section>
+            <Footer />
+            
+        </div>
+    )
+}
